40be5c3cf5bef9e81fa55eed39c6b9dda503f28a
Tomaz Kragelj <tkragelj@gmail.com>
2014-06-11 22:32:16 +0200

    Implemented symbolication for known addresses.

--- symbolicator/FileSymbolicator.swift
+++ symbolicator/FileSymbolicator.swift
@@ -8,40 +8,93 @@ Copyright (c) 2014 Gentle Bytes. All rights reserved.
 import Foundation
 
 class FileSymbolicator {
-	func symbolicate(contents: String, archiveHandler: ArchiveHandler) {
+	func symbolicate(contents: String, archiveHandler: ArchiveHandler) -> String? {
 		let optionalInformation = self.extractProcessInformation(contents)
 		if !optionalInformation {
-			return;
+			return nil
 		}
 		
+		// Unwrap information tuple from optional value.
 		let information = optionalInformation!
 		
 		// Get starting address.
-		let optionalStartAddress = self.baseAddress(contents, identifier: information.identifier, version: information.version, build: information.build)
+		let optionalStartAddress = self.matchBaseAddressForSymbolication(contents, identifier: information.identifier, version: information.version, build: information.build)
 		if !optionalStartAddress {
-			return
+			return nil
 		}
 
 		// Get the corresponding dwarf file.
 		let optionalDwarfPath = archiveHandler.dwarfPathWithIdentifier(information.identifier, version: information.version, build: information.build)
 		if !optionalDwarfPath {
 			println("ERROR: No archive found!")
-			return
+			return nil
 		}
 		
-		// Find all matches in the crash log, using both, application name and identifier.
+		// Find all matches in the crash log, using both, application name and identifier. Group 1 contains address, group 2 text to be replaced with symbolized location.
 		var matches = RxMatch[]()
-		matches += self.matches(contents, identifier: information.name)
-		matches += self.matches(contents, identifier: information.identifier)
+		matches += self.matchSymbolsForSymbolication(contents, identifier: information.name)
+		matches += self.matchSymbolsForSymbolication(contents, identifier: information.identifier)
+		println("Matched \(matches.count) addresses for symbolizing")
+		
+		// Extract array of addresses that need symbolication and symbolize them.
+		let addresses = matches.map { $0.groups[1].value } as String[]
+		let symbols = self.symbolicateAddresses(optionalStartAddress!, dwarfPath: optionalDwarfPath!, addresses: addresses)
+		let symbolizedString = self.generateSymbolicatedString(contents, matches: matches, symbols: symbols)
+		return symbolizedString
 	}
 	
-	/* private */ func matches(contents: NSString, identifier: String) -> RxMatch[] {
-		let pattern: NSString = "^[0-9]+\\s+\(identifier)\\s+(0x[0-9a-fA-F]+)(.+)$"
+	/* private */ func generateSymbolicatedString(contents: String, matches: RxMatch[], symbols: String[]) -> String {
+		var previousIndex = 0
+		var rawContents = contents.bridgeToObjectiveC()
+		var symbolizedContents = String()
+		
+		// Iterate over all parts that need symbolication.
+		for (index, match) in enumerate(matches) {
+			let symbol = symbols[index]
+			let replaceRange = match.groups[2].range!
+			
+			// Get the substring between previous match and current one.
+			let skippedRange = NSRange(location: previousIndex, length: replaceRange.location - previousIndex)
+			let skippedContents = rawContents.substringWithRange(skippedRange)
+			
+			// Append skipped substring and symbol.
+			symbolizedContents += skippedContents
+			symbolizedContents += symbol
+			
+			previousIndex = replaceRange.location + replaceRange.length
+		}
+		
+		// Symbolicate remaining string.
+		if previousIndex < countElements(contents) {
+			symbolizedContents += rawContents.substringFromIndex(previousIndex)
+		}
+		
+		return symbolizedContents
+	}
+	
+	/* private */ func symbolicateAddresses(baseAddress: String, dwarfPath: String, addresses: String[]) -> String[] {
+		let stdOutPipe = NSPipe()
+		let stdErrPipe = NSPipe()
+		let task = NSTask()
+		task.launchPath = "/usr/bin/xcrun"
+		task.arguments = ["atos", "-arch", "x86_64", "-o", dwarfPath, "-l", baseAddress] + addresses
+		task.standardOutput = stdOutPipe
+		task.standardError = stdErrPipe
+		task.launch()
+		task.waitUntilExit()
+		
+		let translatedData = stdOutPipe.fileHandleForReading.readDataToEndOfFile()
+		let translatedString = NSString(data: translatedData, encoding: NSASCIIStringEncoding)
+		return translatedString.componentsSeparatedByString("\n") as String[]
+	}
+	
+	/* private */ func matchSymbolsForSymbolication(contents: NSString, identifier: String) -> RxMatch[] {
+		let pattern: NSString = "^[0-9]+\\s+\(identifier)\\s+(0x[0-9a-fA-F]+)\\s+(.+)$"
 		let regex = pattern.toRxWithOptions(NSRegularExpressionOptions.AnchorsMatchLines)
 		return contents.matchesWithDetails(regex) as RxMatch[]
 	}
 	
-	/* private */ func baseAddress(contents: String, identifier: String, version: String, build: String) -> String? {
+	/* private */ func matchBaseAddressForSymbolication(contents: String, identifier: String, version: String, build: String) -> String? {
 		let pattern: NSString = "^\\s+(0x[0-9a-fA-F]+)\\s+-\\s+(0x[0-9a-fA-F]+)\\s+[+]?\(identifier)\\s+\\(\(version)\\s*-\\s*\(build)\\)"
 		let optionalMatch = pattern.toRxWithOptions(NSRegularExpressionOptions.AnchorsMatchLines)!.firstMatchWithDetails(contents)
 		if !optionalMatch {
@@ -53,10 +106,6 @@ class FileSymbolicator {
 		println("Starting address is \(result)")
 		return result
 	}
-	
-	// xcrun atos -arch x86_64
-	// -o "~/Library/Developer/Xcode/Archives/2013-12-18/Startupizer 2.3.1 (1980) 18.12.13 07.49.xcarchive/dSYMs/Startupizer2.app.dSYM/Contents/Resources/DWARF/Startupizer2" 
-	// -l
 	
 	/* private */ func extractProcessInformation(contents: String) -> (name: String, identifier: String, version: String, build: String)? {
 		let optionalProcessMatch = "^Process:\\s+([^\\[]+) \\[[^\\]]+\\]".toRxWithOptions(NSRegularExpressionOptions.AnchorsMatchLines)!.firstMatchWithDetails(contents)
--- symbolicator/Symbolicator.swift
+++ symbolicator/Symbolicator.swift
@@ -16,6 +16,7 @@ class Symbolicator {
 		
 		for filename in files {
 			// Prepare full path to crash log and bail out if it doesn't exist.
+			println()
 			println("Symbolicating \(filename)...")
 			let path: String = filename.stringByStandardizingPath
 			if !NSFileManager.defaultManager().fileExistsAtPath(path) {
@@ -27,11 +28,18 @@ class Symbolicator {
 			let optionalContents = String.stringWithContentsOfFile(path, encoding: NSUTF8StringEncoding, error: nil)
 			if (!optionalContents) {
 				println("ERROR: can't read contents of \(path.lastPathComponent)!")
-				continue;
+				continue
 			}
 			
 			// Symbolicate the crash log.
-			symbolicator.symbolicate(optionalContents!, archiveHandler: archiveHandler)
+			if let symbolized = symbolicator.symbolicate(optionalContents!, archiveHandler: archiveHandler) {
+				if !symbolized.writeToFile(path, atomically: true, encoding: NSUTF8StringEncoding, error: nil) {
+					println("ERROR: failed saving symbolized contents!")
+					continue
+				}
+			}
+			
+			println("Finished!")
 		}
 	}
 }
